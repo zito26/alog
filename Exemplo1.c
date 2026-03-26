feb8b541debf43a4eec2551a4621c98ec2868e8f
zito26 <vitoraraujopiovesan9@gmail.com>
2026-03-25 22:07:23 -0300

    Add files via upload

---
+++ Exemplo1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+void main(void)
+{
+    printf("Jonathan\nCalleri\n");
+    printf("\tLinguagem C");
+}
---
+++ Exemplo2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+void main(void)
+{
+printf("um\n");
+printf("\tdois\n");
+printf("\t\ttres");
+}
