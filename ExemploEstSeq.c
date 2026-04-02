05a434b295e865dd7ff7c838213a460cba7cfdf4
zito26 <vitoraraujopiovesan9@gmail.com>
2026-04-01 21:59:11 -0300

    Add files via upload

---
+++ ExemploEstSeq.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main(void)
+{
+ int Idade, TotalDias;
+ printf("Insira sua idade: ");
+ scanf("%i", &Idade);
+ TotalDias = Idade * 365;
+ printf("Sua idade em dias: %i", TotalDias);
+
+
+}
